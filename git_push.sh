3ee5e90d43c4bc6e54c2b7ad3f802be60224b5ec
Isaac Thani <thanimosesisaac3@gmail.com>
2021-07-05 11:00:21 +0300

    upd bsh

--- git_push.sh
+++ git_push.sh
@@ -1,12 +1,20 @@
-echo "Enter your message"
-read message
-git add .
-git commit -m"${message}"
-if [ -n "$(git status - porcelain)" ];
-then
- echo "IT IS CLEAN"
-else
- git status
- echo "Pushing data to remote server!!!"
- git push -u origin master
-fi
+#!/bin/bash
+read -p "Commit description: " desc
+git add . && \
+git add -u && \
+git commit -m "$desc" && \
+git push heroku master
+
+# -------
+# echo "Enter your message"
+# read message
+# git add .
+# git commit -m"${message}"
+# if [ -n "$(git status - porcelain)" ];
+# then
+#  echo "IT IS CLEAN"
+# else
+#  git status
+#  echo "Pushing data to remote server!!!"
+#  git push -u origin master
+# fi
